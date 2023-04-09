530ec1f83c5ebbc229351cb9a8d9905dec6add21
frontweb-dev <frontend_web@kakao.com>
2023-04-09 17:58:41 +0900

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ webtoon }: Webtoons) => {
   return (
-    <Link to={`/${webtoon.service}/${webtoon.title}`} key={webtoon._id}>
-      <img
-        className='h-40 w-full border border-slate-200 object-cover'
-        src={webtoon.img}
-        alt={webtoon.title}
-      />
+    <Link to={`/${webtoon.service}/${webtoon.title}`} key={webtoon._id} className=''>
+      <div className='h-40 w-full'>
+        <img
+          className='h-full w-full border border-slate-200 object-cover'
+          src={webtoon.img}
+          alt={webtoon.title}
+        />
+      </div>
       <h3 className='text-elipse mt-2 text-sm font-bold leading-4 '>{webtoon.title}</h3>
     </Link>
   );
